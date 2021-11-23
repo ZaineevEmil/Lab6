8dce23830c1d2d1cd8e534c9c9157e4ea6e8ce94
2299112@gmail.com <2299112@gmail.com>
2021-11-23 23:06:15 +0500

    Добавьте файлы проекта.

---
+++ Lab6.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Определение полиндрома
+            Console.WriteLine("Введите предложение без знаков препинания");
+            string predlogenie = Console.ReadLine();
+            string predlogenieBezProbelov = predlogenie.Replace(" ", "").ToLower();
+            int n = predlogenieBezProbelov.Length;
+            string nachalo = "";
+            string konec = "";
+            string obratno = "";
+            if (n % 2 == 0)
+            {
+                nachalo = predlogenieBezProbelov.Substring(0, n / 2);
+                konec = predlogenieBezProbelov.Substring(n / 2);
+                char[] konecArray = konec.ToCharArray();
+                Array.Reverse(konecArray);
+                obratno = new string(konecArray);
+            }
+            else
+            {
+                nachalo = predlogenieBezProbelov.Substring(0, (n / 2));
+                konec = predlogenieBezProbelov.Substring(n / 2 + 1);
+                char[] konecArray = konec.ToCharArray();
+                Array.Reverse(konecArray);
+                obratno = new string(konecArray);
+            }
+            int result = String.Compare(nachalo, obratno);
+            if (result == 0)
+            {
+                Console.WriteLine("Строка является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Строка не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Нахождение самого длинного слова
+            Console.WriteLine("Введите предложение без знаков препинания");
+            string predlogenie = Console.ReadLine();
+            int number = 0;
+            int numberMax = 0;
+            string wordMax = "";
+            string[] stringArray = predlogenie.Split();
+            foreach (string s in stringArray)
+            {
+                number = s.Length;
+                while (number > numberMax)
+                {
+                    numberMax = number;
+                    wordMax = s;
+                }
+            }
+            Console.WriteLine("Самое длинное слово - {0}", wordMax);
+            Console.ReadKey();
+        }
+    }
+}
